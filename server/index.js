16126fedbc94e81bc6df15b487aa3a92dadad5c1
Bryanromerou <bryanromerou@gmail.com>
2021-03-11 15:44:18 -0800

    initialize the express server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const app = express();
 const cors = require('cors');
+
+const PORT = 3000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Technical Singularity API</h1>')
+});
+
+// Listen For Requests
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
