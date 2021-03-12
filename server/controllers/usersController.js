4ab4ab2198d94b04effdef3b1ab4119fa4c44e71
Bryanromerou <bryanromerou@gmail.com>
2021-03-12 12:35:10 -0800

    adds create route and deletes many console logs

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,23 +3,21 @@ const db = require("../models");
 // /api/v1/users
 const index = async (req,res)=>{
   const users = await db.user.findAll({attributes: {exclude: ['password','email']}});
-  console.log(users);
   res.json(users)
 }
 
 const show = async (req,res)=>{
   const currentUser = await db.user.findOne({where:{id:req.params.id},attributes: {exclude: ['password','email']}});
-  console.log(currentUser)
   res.json(currentUser)
 }
 
 const create = async (req,res)=>{
-  console.log(req.body)
-  res.send('<h1>This is the Create route</h1>');
+  const newUser = await db.user.create(req.body);
+  const userData = await db.user.findOne({where:{id:newUser.id},attributes: {exclude: ['password','email']}})
+  res.json({'Added User':userData});
 }
 
 const destroy = async (req,res) =>{
-  console.log(req.params.id);
   const deletedUser = await db.user.findOne({where:{id:req.params.id},attributes: {exclude: ['password','email']}});
   await deletedUser.destroy();
   res.json({"Deleted User":deletedUser});
@@ -28,5 +26,6 @@ const destroy = async (req,res) =>{
 module.exports = {
   index,
   show,
+  create,
   destroy,
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ const controllers = require('../controllers');
 
 router.get('/',controllers.usersCtrl.index);
 router.get('/:id',controllers.usersCtrl.show);
+router.post('/',controllers.usersCtrl.create);
 router.delete('/:id',controllers.usersCtrl.destroy);
 
 module.exports = router;
